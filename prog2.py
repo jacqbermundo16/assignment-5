c7613553a950d6c490cde25dbe714e274cabd617
Jacqueline C Bermundo <92629695+jacqbermundo16@users.noreply.github.com>
2021-11-19 23:12:22 +0800

    Displays the second input if it's the lowest number

--- prog2.py
+++ prog2.py
@@ -12,14 +12,21 @@ def askInputs():
     return firstNumber, secondNumber, thirdNumber
 
 #2 create if else statement to determine the lowest number
-#3 display the number with the lowest value
 def lowestOfThree(a_, b_, c_):
+    #2.1 If a is the lowest number
     if a_ < b_ and a_ < c_:
         print (f"Lowest Number is {a_}. ")
+
+    #2.2 If b is the lowest number
+    elif b_ < a_ and b_ < c_:
+        print (f"Lowest Number is {b_}. ")
+    
+    #2.3 If c is the lowest number
     else:
         print ('others')
 
 
 
 a, b, c = askInputs()
+#3 display the number with the lowest value
 lowestOfThree(a, b, c)
