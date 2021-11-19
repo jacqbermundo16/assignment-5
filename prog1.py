4bf5541e5a80a1e42680faec285f51aaf1102028
Jacqueline C Bermundo <92629695+jacqbermundo16@users.noreply.github.com>
2021-11-19 21:38:01 +0800

    Displays the input grade that is equivalent to 2.50

--- prog1.py
+++ prog1.py
@@ -40,10 +40,15 @@ def convert(inputGradef):
    # 2.6      82-84 = 2.25  Good
     elif round(inputGrade_) >= 82 and round(inputGrade_) <= 84:
         print(f" Input Grade: {inputGrade_: .2f}\n Grade/Mark: 2.25\n Description: Good ")
+
+   # 2.7      79-81 = 2.50  Satsifactory
+    elif round(inputGrade_) >= 79 and round(inputGrade_) <= 81:
+        print(f" Input Grade: {inputGrade_: .2f}\n Grade/Mark: 2.50\n Description: Satisfactory ")
+
     else: 
         print('others')    
 
-   # 2.7      79-81 = 2.50  Satsifactory
+  
    # 2.8      76-78 = 2.75  Satisfactory
    # 2.9         75 = 3.00  Passing
    # 2.10     65-74 = 5.00  Failure
